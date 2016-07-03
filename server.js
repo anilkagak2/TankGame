80c945c9d23b2b9e0858c3535594a1e4a8603903
Anil Kag <anilkagak2@gmail.com>
2016-07-03 11:34:19 +0530

    Delivering compressed content using gzip

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var app = express();
+
+// New call to compress content
+app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);
